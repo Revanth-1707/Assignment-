32a212789836ba51d32635d01671311a97d9be92
Revanth-1707 <85238812+Revanth-1707@users.noreply.github.com>
2022-07-28 15:55:39 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+var country = { 
+
+       canberra: {  
+
+            population: 18000
+
+       },
+       Washington: {  
+
+            population: 17000
+       },
+       London: {  
+
+            population: 22000
+       },
+       Moscow: {  
+
+            population: 24000
+       }
+}
+for(i=0,i<=length.country,i++){
+if country[i].population > 20000{
+print(country[i]
+
+
+
+       
+
